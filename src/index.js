27d7b9c17123a634668db416983736945c944634
ynitish739 <nitish12a4372@gmail.com>
2024-12-03 18:06:15 +0530

    init4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import GlobalStyle from './theme/GlobalStyle';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <GlobalStyle />
+//     <App />
+//   </React.StrictMode>
+// );
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import GlobalStyle from './theme/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <BrowserRouter basename="/coolfolio">  {/* Add basename */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
